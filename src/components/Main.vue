1c7d2f97943c25414aaf6f1e57e1fc57adcbcefa
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-02-16 14:45:23 +0000

    ACQUI-8: Add base Vue app and webpack config

---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="main container-fluid">
+        <div class="row">
+            <div class="col-sm-10 col-sm-push-2">
+                <main>
+                    <router-view :key="$route.path" />
+                </main>
+            </div>
+            <div class="col-sm-2 col-sm-pull-10">
+                <NavMenu />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import NavMenu from './NavMenu.vue'
+
+export default {
+    components: {
+        NavMenu,
+    }
+}
+</script>
+
+<style>
+
+</style>
