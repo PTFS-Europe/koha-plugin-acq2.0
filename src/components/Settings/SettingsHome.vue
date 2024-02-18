b634ff7122bb1809764a4fafbaebaf582b688c0c
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-02-18 17:39:22 +0000

    ACQUI-24: Add navigation panes to settings home page

--- src/components/Settings/SettingsHome.vue
+++ src/components/Settings/SettingsHome.vue
@@ -1,13 +1,59 @@
 <template>
-  <h1>Settings</h1>
+    <h1>Settings</h1>
+    <div class="navPanesGrid">
+        <SettingsCard
+            v-for="(item, key) in navPanes"
+            v-bind:key="key"
+            :item="item"
+        ></SettingsCard>
+    </div>
 </template>
 
 <script>
-export default {
+import settingsJSON from '../../../Koha/Plugin/Acquire/installer/sysprefs/sysprefs.json'
+import SettingsCard from './SettingsCard.vue'
 
+export default {
+    setup() {
+        return {
+            settingsJSON
+        }
+    },
+    data() {
+        return {
+            navPanes: []
+        }
+    },
+    beforeRouteEnter(to, from, next) {
+        next(vm => {
+            vm.createNavPanes(settingsJSON)
+        })
+    },
+    methods: {
+        createNavPanes(settingsJSON) {
+            const modules = Object.keys(settingsJSON)
+            const navPanes = modules.map(module => {
+                const moduleData = settingsJSON[module]
+                return {
+                    path: "/acquisitions/settings/" + module,
+                    icon: moduleData.icon,
+                    title: moduleData.title,
+                }
+            })
+            this.navPanes = navPanes
+            return navPanes
+        }
+    },
+    components: { SettingsCard },
 }
 </script>
 
 <style>
-
+.navPanesGrid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    padding: 1em;
+    gap: 3em;
+    width: 90%;
+}
 </style>
