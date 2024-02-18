369243285ed4deca82a8e5b100479a241280e279
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-02-18 11:47:23 +0000

    ACQUI-21: Add pinia store and navigation setup

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,11 +3,13 @@
         <div class="row">
             <div class="col-sm-10 col-sm-push-2">
                 <main>
-                    <router-view :key="$route.path" />
+                    <router-view :key="$route.name" />
                 </main>
             </div>
             <div class="col-sm-2 col-sm-pull-10">
-                <NavMenu />
+                <NavMenu
+                    :title="'Acquisitions'"
+                ></NavMenu>
             </div>
         </div>
     </div>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,33 @@
-import { createApp } from "vue";
-import { createWebHistory, createRouter } from "vue-router";
+import { createApp } from "vue"
+import { createWebHistory, createRouter } from "vue-router"
+import { createPinia } from "pinia"
 
-import App from "./components/Main.vue";
-import { routes } from './routes/routes.js'
+import App from "./components/Main.vue"
+import { routes as routesDef } from './routes/routes.js'
+import { useMainStore } from "./stores/main"
+import { useNavigationStore } from "./stores/navigation"
+
+const pinia = createPinia()
+const mainStore = useMainStore(pinia)
+const navigationStore = useNavigationStore(pinia)
+const routes = navigationStore.setRoutes(routesDef)
 
 const router = createRouter({
     history: createWebHistory(),
     linkActiveClass: "current",
     routes,
-});
+})
 
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
+app.provide("navigationStore", navigationStore)
+
 
 app.mount("#__app")
+
+const { removeMessages } = mainStore;
+router.beforeEach((to, from) => {
+    navigationStore.$patch({ current: to.matched, params: to.params || {} })
+    removeMessages(); // This will actually flag the messages as displayed already
+})
