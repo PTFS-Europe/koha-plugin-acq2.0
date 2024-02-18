5a850b357f50ec0cdc54b0107c7b0719a871d11e
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-02-18 16:45:30 +0000

    ACQUI-19: Add re-useable Koha core components

---
+++ src/components/Help.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav id="shortcut" class="shortcut">
+        <a
+            href="/cgi-bin/koha/help.pl"
+            class="toplinks"
+            id="helper"
+            target="_blank"
+            ><i class="fa fa-question-circle"></i> <span>Help</span></a
+        >
+    </nav>
+</template>
+
+<script></script>
