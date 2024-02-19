b16bbf38e965847b61b200f1ce35b8d333d7b0c1
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-02-19 14:51:36 +0000

    ACQUI-34: Pass user's library groups on page load

--- Koha/Plugin/Acquire/views/acquisitions.pl
+++ Koha/Plugin/Acquire/views/acquisitions.pl
@@ -25,6 +25,8 @@ use C4::Output    qw( output_html_with_http_headers );
 use CGI qw ( -utf8 );
 
 use Koha::Plugin::Acquire;
+use Koha::Patrons;
+use Koha::Libraries;
 
 my $self  = Koha::Plugin::Acquire->new;
 my $query = CGI->new;
@@ -38,11 +40,72 @@ my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
 );
 
 my $patron = Koha::Patrons->find({ borrowernumber => $borrowernumber });
-
 my $userflags = haspermission($patron->userid);
 
+# my $branch = Koha::Libraries->find({ branchcode => $patron->branchcode });
+
+my $acquisitions_library_groups = Koha::Library::Groups->search({ ft_acquisitions => 1 });
+my @user_library_groups;
+
+foreach my $alg ( @{$acquisitions_library_groups->as_list } ) {
+    my $lib_group = _map_library_group({ group => $alg });
+    push @user_library_groups, $lib_group;
+}
+
 $template->param(
-    userflags => $userflags
+    userflags => $userflags,
+    library_groups => \@user_library_groups
 );
 
 output_html_with_http_headers $query, $cookie, $template->output;
+
+sub _map_library_group {
+    my ( $args ) = @_;
+
+    my $group = $args->{group};
+    my $lib_group = {
+        title => $group->title,
+        id    => $group->id,
+    };
+    my @libs_or_sub_groups = Koha::Library::Groups->search(
+        {
+            parent_id       => $group->id
+        }
+    )->as_list;
+
+    my @libraries;
+    my @sub_groups;
+    foreach my $lib ( @libs_or_sub_groups ) {
+        if ($lib->branchcode) {
+            push( @libraries, $lib->unblessed );
+        } else {
+            push( @sub_groups, _map_library_group({ group => $lib }) );
+        }
+    }
+    $lib_group->{libraries} = \@libraries;
+    $lib_group->{sub_groups} = \@sub_groups;
+
+    $lib_group = _assign_branches_to_parent({ group => $lib_group });
+
+    return $lib_group;
+}
+
+sub _assign_branches_to_parent {
+    my ($args) = @_;
+
+    my $group = $args->{group};
+    my $libraries = $group->{libraries};
+    my $sub_groups = $group->{sub_groups};
+
+    if(!scalar(@$libraries) && scalar(@$sub_groups)) {
+        my @sub_group_libraries;
+        foreach my $sub_group (@$sub_groups) {
+            if(!scalar(@{ $sub_group->{libraries} })) {
+                $sub_group = _assign_branches_to_parent({ group => $sub_group });
+            }
+            push(@sub_group_libraries, @{ $sub_group->{libraries} });
+        }
+        $group->{libraries} = \@sub_group_libraries;
+    }
+    return $group;
+}
--- src/stores/acquisitions.js
+++ src/stores/acquisitions.js
@@ -4,8 +4,9 @@ export const useAcquisitionsStore = defineStore("acquisitions", {
     state: () => ({
         user: {
             logged_in_user: null,
-            userflags: null
+            userflags: null,
+            library_groups: null
         },
-        settings: null
+        settings: null,
     })
 });
