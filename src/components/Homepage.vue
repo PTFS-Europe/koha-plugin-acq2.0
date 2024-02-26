98c80cf7df0748943d1f29dbee816970aa0f1dc6
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-02-26 10:53:37 +0000

    ACQUI-81: Add route level permission guards
    This patch adds route level permission guards to routes that require it. If a user who is not permitted to access that route tries to access the page they will be redirected to the homepage and shown a warning to tell them they are not permitted.

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -3,8 +3,27 @@
 </template>
 
 <script>
-export default {
+import { inject } from "vue"
+import { storeToRefs } from "pinia"
+import { setWarning } from "./../messages"
 
+export default {
+    setup() {
+        const acquisitionsStore = inject("acquisitionsStore")
+        const { 
+            navigationBlocked
+        } = storeToRefs(acquisitionsStore)
+
+        return {
+            navigationBlocked
+        }
+    },
+	beforeCreate() {
+		if(this.navigationBlocked) {
+			setWarning("You did not have the required permissions to access that page. Please contact your system administrator.")
+			this.navigationBlocked = false
+		}
+	}
 }
 </script>
 
